88d94c5b1e5099a424800a56afd78b9df0f7edb9
Roshin Prasad <88421254+RoshinPrasad@users.noreply.github.com>
2023-05-27 23:30:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,42 +6,6 @@ const nocache = require('nocache');
 const mongoSanitize = require('express-mongo-sanitize');
 const app = express()
 
-DB = process.env.DBURL
-mongoose.connect(DB)
-
-const connection = mongoose.connection;
-
-connection.once('open', () => {
-  console.log('connection is successfull');
-})
-
-app.set('view engine', 'ejs');
-
-app.use(express.static('public'));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(nocache());
-
-app.use(mongoSanitize());
-
-const userRoute = require("./routes/userRoute");
-app.use("/", userRoute);
-
-const adminRoute = require("./routes/adminRoute");
-app.use("/admin", adminRoute);
-
-const forgotPassword = require("./routes/forgotPassword");
-
-app.use("/forgot", forgotPassword);
-
-app.all("*", (req, res) => {
-  res.render("error")
-})
-
-app.listen(4000, function () {
-  console.log("server is running at 4000");
-});
 
 
 
